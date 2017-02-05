a4ec1e88ea2215d3d0799f1f95e38f533d721ca6
PJang <PJang@DESKTOP-L6QK46M>
2017-02-04 17:54:29 -0800

    Add project files.

---
+++ SpotifyFun/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SpotifyFun.Startup))]
+namespace SpotifyFun
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
