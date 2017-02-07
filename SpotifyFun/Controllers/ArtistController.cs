cbe14d65790a850c469e3386bc6c7025b56b4078
Paul Jang <pjang94@gmail.com>
2017-02-06 23:01:17 -0800

    - song previews are now triggered by a button - searching for an album provides a picture of the album cover as well as previews for each track - searching for an artist provides a picture of the artist

---
+++ SpotifyFun/Controllers/ArtistController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpotifyFun.Controllers
+{
+    public class ArtistController : Controller
+    {
+        public ActionResult SearchArtistByName(string artistName)
+        {
+            string id = GetArtistIDFromName(artistName);
+            dynamic artistJSON = SearchArtistByID(id);
+            TempData["artistJSON"] = artistJSON;
+
+            return View("ArtistInfoPage");
+        }
+
+        public string GetArtistIDFromName(string name)
+        {
+            string url = string.Format("https://api.spotify.com/v1/search?q={0}&type={1}", name, "artist");
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            StreamReader sr = new StreamReader(stream);
+            string jsonString = sr.ReadToEnd();
+
+            dynamic json = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(jsonString);
+
+            return json.artists.items[0].id;
+        }
+
+        public dynamic SearchArtistByID(string id)
+        {
+            string url = string.Format("https://api.spotify.com/v1/{0}/{1}", "artists", id);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            StreamReader sr = new StreamReader(stream);
+            string jsonString = sr.ReadToEnd();
+
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(jsonString);
+        }
+    }
+}
---
+++ SpotifyFun/Scripts/dynamicElements.js
@@ -0,0 +1,3 @@
+﻿function playPreview(divName, url) {
+    document.getElementById(divName).innerHTML = '<iframe type="text/html" width="480" height="75" src='+url+' frameborder="0"></iframe>';
+}
